023c8e34ade1c271f550db106b60249785da83fa
Anxo Portela-Insua Blanco <hello@anxoportela.dev>
2026-02-15 00:24:17 +0100

    Clean: remove generated files

--- docs/.docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require("/home/anxo/Projects/postman-newman-gh/node_modules/infima/dist/css/default/default.css"),
-  require("/home/anxo/Projects/postman-newman-gh/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/anxo/Projects/postman-newman-gh/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-];
